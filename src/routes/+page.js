4bafe9c91303a265a9577792933e02e012b8b0ac
plutoniumm <51614345+plutoniumm@users.noreply.github.com>
2026-05-15 16:40:05 +0530

    bugz

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,5 +1,6 @@
 import { redirect } from '@sveltejs/kit';
+import { base } from '$app/paths';
 
 export function load() {
-  redirect(302, '/aramaic');
+  redirect(302, `${base}/aramaic`);
 }
--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,12 @@
 import adapter from '@sveltejs/adapter-static';
 
+const base = process.env.BASE ?? '';
+
 /** @type {import('@sveltejs/kit').Config} */
 export default {
   kit: {
     adapter: adapter(),
+    paths: { base },
     files: {
       assets: 'public'
     }
